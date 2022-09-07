f6a12f08cc3d1a0f2e7e4251618f02c72bdb5d3f
Brandon Braner <brandon.braner@gmail.com>
2022-09-07 17:52:00 -0500

    wip

--- apps/tailwind-nx-demo/src/app/app.module.ts
+++ apps/tailwind-nx-demo/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderComponent } from './header/header.component';
+import { TopnavComponent } from './topnav/topnav.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TopnavComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/tailwind-nx-demo/src/app/topnav/topnav.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'angular-tailwind-nx-topnav',
+  templateUrl: './topnav.component.html',
+  styleUrls: ['./topnav.component.css'],
+})
+export class TopnavComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
